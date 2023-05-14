965987ed6461ff3e33b1cad369ade59b57960033
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-05-14 01:24:15 -0700

    update interpreter.c

--- interpreter.c
+++ interpreter.c
@@ -1,18 +1,6 @@
 #include "shell.h"
 
 /**
- * handle_signal - Just catch the signal SIGNINT when a user click on
- * Crtl+C
- * @s: the signal type int
- * Return: None (SUCCESS)
- */
-void handle_signal(int s)
-{
-	putchar('\n');
-	exit(EXIT_SUCCESS || ~s);
-}
-
-/**
  * interpreter - UNIX command line interpreter
  * @argc: the number of arguments of command line(argc donne
  * le nombre d'éléments de la ligne de commande)
@@ -22,36 +10,31 @@ void handle_signal(int s)
  */
 void interpreter(int argc, char *argv[])
 {
-	char *filename = malloc(sizeof(char));
-	char *environ[] = { NULL };
-	int status = 0;
+	char *args[] = { NULL, NULL };
+	char cmd[1024];
+	char *env[] = { NULL };
 
-	signal(SIGINT, handle_signal);
 	while (argc || 1)
 	{
-		pid_t p = fork();
-
-		if (p == 0)
+		printf("#cisfun$ ");
+		fflush(stdout);
+		if (fgets(cmd, sizeof(cmd), stdin) != NULL)
 		{
-			printf("#cisfun$ ");
-			scanf("%s", filename);
-			if (execve(filename, argv, environ) == -1)
-				perror(argv[0]);
+			cmd[strcspn(cmd, "\n")] = '\0';
+			args[0] = cmd;
+			if (fork() == 0)
+			{
+				if (execve(args[0], args, env) == -1)
+				{
+					perror(argv[0]);
+					exit(EXIT_FAILURE);
+				}
+			}
+			else
+				wait(NULL);
 		}
-		else if (p > 0)
-			waitpid(p, &status, 0);
 		else
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-
-		if (feof(stdin))
-		{
-			putchar('\n');
 			break;
-		}
 
 	}
-	free(filename);
 }
