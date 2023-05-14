c060b9a03828f441a8c3a155a5f78c5597998207
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-05-14 01:33:10 -0700

    end simple shell 0.1

--- interpreter.c
+++ interpreter.c
@@ -2,6 +2,9 @@
 
 /**
  * interpreter - UNIX command line interpreter
+ *
+ * SIMPLE SHELL 0.1
+ *
  * @argc: the number of arguments of command line(argc donne
  * le nombre d'éléments de la ligne de commande)
  * @argv: the array of string(argv contient ces éléments sous
