d6709c66bdab834b5a6d9aa135a8668083996db3
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-05-24 14:22:59 -0700

    main shell

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * main - test main interpreter
+ * @argv: ...
+ * @argc: ...
+ * Return: (SUCCESS 0)
+ */
+int main(int argc, char *argv[])
+{
+	interpreter(argc, argv);
+	return (0);
+}
